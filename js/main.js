8d9a4b33a3b234fa54ef5c751e667621be25e23f
ShinVdm <122155790+ShinVdm@users.noreply.github.com>
2023-02-04 03:31:38 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-const burger = document.querySelector('.burger');
-const menuMobile = document.querySelector('.menu_mobile');
-
-burger.addEventListener('click', () =>{
-  menuMobile.classList.toggle('menu_open');
+$('.burger').on('click', function() {
+  document.body.classList.toggle('_lock');
+  $('.menu_mobile').toggleClass('menu_open');
 });
+$('.burger').click(function(){
+  $('.span_burger:nth-child(1)').toggleClass('first');
+  $('.span_burger:nth-child(2)').toggleClass('middle');
+  $('.span_burger:nth-child(3)').toggleClass('last');
+});
+$( '.menu_mobile li a' ).on("click", function(){
+  $('.menu_mobile').removeClass('menu_open');
+  $('body').removeClass('_lock');
+  $('.span_burger:nth-child(1)').removeClass('first');
+  $('.span_burger:nth-child(2)').removeClass('middle');
+  $('.span_burger:nth-child(3)').removeClass('last');
+});
+
+
+
 
 var swiper = new Swiper(".mySwiper", {
     effect: "cards",
